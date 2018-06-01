d9bff53f6b6c77ad48897419ca3328b92e053612
fenxiang.zhang <fenxiang.zhang@wwwarehouse.com>
2018-06-01 15:02:54 +0800

    编码结果Coded在初始化时,需要输入解释器

--- encode/coded/UniqueCoded.py
+++ encode/coded/UniqueCoded.py
@@ -1,3 +1,4 @@
+from encode.interpreter.UniqueInterpreter import UniqueInterpreter
 from encode.list.UniqueList import UniqueList
 
 
@@ -6,9 +7,9 @@ class UniqueCoded:
     编码器编码结果
     """
 
-    def __init__(self):
+    def __init__(self, interpreter: UniqueInterpreter):
         super().__init__()
         # 编码原数据:{编码后数据的索引:原始数据}
-        self.records = UniqueList()
+        self.records = UniqueList(interpreter)
         # 数据的唯一编码,及其对应的数据索引
         self.uniques = {}
