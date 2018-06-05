2635ab85a084c1b994f13560d8f3f495375bdcd7
fenxiang.zhang <fenxiang.zhang@wwwarehouse.com>
2018-06-05 11:36:22 +0800

    重定义压缩器

--- encode/compressor/ICompressor.py
+++ encode/compressor/ICompressor.py
@@ -1,10 +1,13 @@
+from encode.vector.Vector import Vector
+
+
 class ICompressor:
     """
-    数据压缩器
+    数据压缩器接口
     """
 
     @staticmethod
-    def compress(records: list) -> list:
+    def compress(records: Vector) -> list:
         """
         对数据进行压缩
         :param records:
