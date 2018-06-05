5d8545fd4a260b571615ddb28326c759caebde5b
fenxiang.zhang <fenxiang.zhang@wwwarehouse.com>
2018-06-05 11:37:17 +0800

    维度投影变更为独热投影

--- encode/projector/OneHotProjector.py
+++ encode/projector/OneHotProjector.py
@@ -1,56 +1,67 @@
 from numpy import zeros
 
-from encode.combiner.DefaultCombiner import DefaultCombiner
+from encode.code.DimensionCode import DimensionCode
+from encode.decoding.IDecoding import IDecoding
+from encode.interpreter.DefaultInterpreter import DefaultInterpreter
 from encode.projector.IProjector import IProjector
 
 
-class DimensionProjector(IProjector):
+class OneHotProjector(IDecoding, IProjector):
     """
-    数据按值进行多维投影
+    数据按 维度+值 进行多维投影
     """
 
-    @staticmethod
-    def project(records: list, dimensions: list, projects: dict, combiner=DefaultCombiner):
+    def __init__(self, interpreter: DefaultInterpreter):
+        super().__init__()
+        self.interpreter = interpreter
+        self.projects = {}
+
+    def feed(self, records: list):
         """
         访问每个记录(records),以维度+值(record[dimension])的方式将编码添加至原编码结果(projects)中
         :param records:
-        :param dimensions:
-        :param projects:
-        :param combiner:
         :return:
         """
+        # 投影维度
+        dimensions = self.interpreter.dimensions
+        # 投影值
+        projects = self.projects
+
         for record in records:  # type:dict
             # 所有维度
             for dimension in dimensions:  # type:str
                 # 投影码
-                project = combiner.combine([dimension, str(record[dimension])])
+                project = DimensionCode.code({dimension: str(record[dimension])})
                 # 判断是否存在
                 if project not in projects:
                     projects[project] = len(projects)
 
-    @staticmethod
-    def projecting(records: list, dimensions: list, projects: dict, projected: list, combiner=DefaultCombiner):
+    def project(self, records: list):
         """
         将数据的维度信息(records+dimensions)基于projects进行映射,将结果存储在projects中
         并该次数据的编码结果返回
         :param records:
-        :param dimensions:
-        :param projects:
-        :param projected:二维数组
-        :param combiner:
         :return:
         """
+        #
+        projected = []
+        # 投影维度
+        dimensions = self.interpreter.dimensions
+        # 投影值
+        projects = self.projects
         # 依次对比编码对象,匹配成功则匹配下一个
         for record in records:
             # 编码结果
-            record_coded = zeros((len(projects)))
+            record_coded = zeros((len(projects)),dtype=int)
             # 所有维度
             for dimension in dimensions:
                 # 维度映射码
-                project = combiner.combine([dimension, str(record[dimension])])
+                project = DimensionCode.code({dimension: str(record[dimension])})
                 project_index = projects[project]
                 # 将该标识码置1
                 record_coded[project_index] = 1
 
             # 新增该记录
-            projected.extend([record_coded])
+            projected.extend([record_coded.tolist()])
+
+        return projected
